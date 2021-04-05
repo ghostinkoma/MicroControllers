569bd9130f74c1ef260c86062500c5e0009ae679
ghostinkoma <ghostinkoma@gmail.com>
2021-04-06 00:01:21 +0900

    Update main.c

--- PIC/PIC10F322/WS2812C_2020_test/main.c
+++ PIC/PIC10F322/WS2812C_2020_test/main.c
@@ -13,22 +13,76 @@
 #pragma config LVP = OFF        // Low-Voltage Programming Enable (High-voltage on MCLR/VPP must be used for programming)
 
 #define LED_Length 10
-volatile unsigned int * LED_R[10];
-volatile unsigned int * LED_G[10];
-volatile unsigned int * LED_B[10];
+#define UP 1
+#define DOWN 0
+#define DlayMax 60000
+#define DlayStep 100
+#define DlayMinimun 110
 
+void setDefaltColor();
+void setColor1();
+void Led_Scroll_UP();
+void Led_Scroll_DOWN();
+void SystemSetip();
+void SendLED();
+
+volatile unsigned char LED_Value_Tmp;
 volatile unsigned int LEDR_Value_Tmp;
 volatile unsigned int LEDG_Value_Tmp;
 volatile unsigned int LEDB_Value_Tmp;
-volatile unsigned int bitCount;
+volatile unsigned char bitCount;
+volatile unsigned char Ptern = 0;
+volatile unsigned char UP_DOWN;
+
+volatile unsigned char * LED_R[LED_Length];
+volatile unsigned char * LED_G[LED_Length];
+volatile unsigned char * LED_B[LED_Length];
+
+void main(void){
+    unsigned int delay_interval_count;
+    unsigned int delay_interval;
+    
+    SystemSetip();
+    setDefaltColor();
+    delay_interval = DlayMax;
+    Ptern =  0;
+    while (1){
+        SendLED();
+        
+        for(delay_interval_count = 0;delay_interval > delay_interval_count; delay_interval_count++){
+            asm("nop");
+        }
+        if(delay_interval < DlayMinimun){
+            delay_interval = DlayMax;
+            if(UP_DOWN == DOWN){
+                asm("bsf    _UP_DOWN, 0");
+                if(Ptern == 0){
+                    setDefaltColor();
+                }else{
+                    setColor1();
+                }
+                if(Ptern >6) Ptern =0; else Ptern ++;
+            }else{
+                asm("bcf    _UP_DOWN, 0");
+            }
+        }else{
+            delay_interval = delay_interval - DlayStep;
+        }
+        
+        if(UP_DOWN == UP){
+            Led_Scroll_UP();
+        }else{
+            Led_Scroll_DOWN();
+        }
+    }
+}
 
 void SendLED(){
-    unsigned int i =0;
-    for(i = 0; i < LED_Length; i ++){
-        LEDG_Value_Tmp = LED_G[i];
-        LEDR_Value_Tmp = LED_R[i];
-        LEDB_Value_Tmp = LED_B[i];
-
+    for(unsigned int Led_Index = 0;Led_Index < 10;Led_Index++){
+        LEDG_Value_Tmp = LED_G[Led_Index];
+        LEDR_Value_Tmp = LED_R[Led_Index];
+        LEDB_Value_Tmp = LED_B[Led_Index];
+        
         asm("bsf    _bitCount, 3");//	; 1
         asm("BitLoopG:");
         asm("BTFSC  _LEDG_Value_Tmp,7");
@@ -60,52 +114,12 @@ void SendLED(){
         asm("GOTO	BitLoopB");
     }
 }
-void setDefaltColor(){
-    LED_R[0] = 100;
-    LED_G[0] = 0;
-    LED_B[0] = 0;
-        
-    LED_R[1] = 100;
-    LED_G[1] = 0;
-    LED_B[1] = 100;
-        
-    LED_R[2] = 50;
-    LED_G[2] = 0;
-    LED_B[2] = 100;
 
-    LED_R[3] = 0;
-    LED_G[3] = 0;
-    LED_B[3] = 200;
-
-    LED_R[4] = 0;
-    LED_G[4] = 50;
-    LED_B[4] = 100;
-        
-    LED_R[5] = 0;
-    LED_G[5] = 250;
-    LED_B[5] = 0;
-        
-    LED_R[6] = 100;
-    LED_G[6] = 70;
-    LED_B[6] = 0;
-        
-    LED_R[7] = 120;
-    LED_G[7] = 50;
-    LED_B[7] = 50;
-        
-    LED_R[8] = 100;
-    LED_G[8] = 100;
-    LED_B[8] = 100;
-        
-    LED_R[9] = 0;
-    LED_G[9] = 0;
-    LED_B[9] = 0;
-}
-void main(void){
+void SystemSetip(){
     // initialize the device
     OSCCON = 0x70;          //IRCF 16MHz; 
-    OPTION_REG = 0x07;				//8M / 256
-    //CLKRCON = 0x00;         //CLKROE disabled; 
+    OPTION_REG = 0x07;		//8M / 256
+    //CLKRCON = 0x00;       //CLKROE disabled; 
     BORCON = 0x00;          // SBOREN disabled; BORFS disabled; BORRDY BOR Circuit is inactive; 
     WDTCON = 0x16;          // WDTPS 1:65536; SWDTEN OFF; 
     LATA = 0x00;            //LATx registers
@@ -113,34 +127,108 @@ void main(void){
     ANSELA = 0x00;          //ANSELx registers
     WPUA = 0x00;            //WPUx registers
     OPTION_REGbits.nWPUEN = 1;
-    unsigned int i;
-    unsigned long delay_interval_count;
-    unsigned long delay_interval;
-    
-     delay_interval = 25535;
-    setDefaltColor();
-    
-    while (1){
-        SendLED();
+}
 
-        for(delay_interval_count = 0;i < delay_interval; i++){
-            asm("nop");
-        }
-        if(delay_interval < 100){
-            delay_interval = 25500;
-        }else{
-            delay_interval = delay_interval - 50;
-        }
-        LEDR_Value_Tmp = LED_R[0];
-        LEDG_Value_Tmp = LED_G[0];
-        LEDB_Value_Tmp = LED_B[0];
-        for( i = 0 ; i < LED_Length -1; i++){
-            LED_R[i] = LED_R[i + 1];
-            LED_G[i] = LED_G[i + 1];
-            LED_B[i] = LED_B[i + 1];
-        }
-        LED_R[9] = LEDR_Value_Tmp;
-        LED_G[9] = LEDG_Value_Tmp;
-        LED_B[9] = LEDB_Value_Tmp;
+void setDefaltColor(){
+ 
+    LED_G[0] = LED_B[0] = 
+    LED_G[1] = 
+    LED_G[2] = 
+    LED_R[3] = LED_G[3] = 
+    LED_R[4] = 
+    LED_R[5] = LED_B[5] = 
+    LED_B[6] =
+    LED_R[9] = LED_G[9] = LED_B[9] = 0;
+    
+    LED_R[2] = 20;
+    
+    LED_R[1] = LED_G[4] =  LED_G[7] = LED_B[7] =  LED_R[0] = LED_G[6] = 55;
+    
+    //= 65;
+
+    LED_B[1] = LED_B[4] = LED_G[5] = LED_R[6] = 
+    LED_R[8] = LED_G[8] = LED_B[8] = 100;
+        
+    LED_R[7] = 120;
+    
+    LED_B[2] = 200;
+    LED_B[3] = 255;
+}
+
+void setColor1(){
+
+    LED_Value_Tmp = Ptern;
+    asm("BTFSC	_LED_Value_Tmp,0"); 
+    asm("GOTO   RedFF");
+    asm("GOTO   Red00");
+    asm("RedFF:");
+    asm("MOVLW  0xFF");
+    asm("MOVWF  _LEDR_Value_Tmp");
+    asm("GOTO   SetGreen");
+    asm("Red00:");
+    asm("CLRF   _LEDR_Value_Tmp");
+    
+    asm("SetGreen:");
+    asm("BTFSC	_LED_Value_Tmp,1"); 
+    asm("GOTO GreenFF");
+    asm("GOTO Green00");
+    asm("GreenFF:");
+    asm("MOVLW  0xFF");
+    asm("MOVWF  _LEDG_Value_Tmp");
+    asm("GOTO SetBlue");
+    asm("Green00:");
+    asm("CLRF    _LEDG_Value_Tmp");
+    
+    asm("SetBlue:");
+    asm("BTFSC	_LED_Value_Tmp,2"); 
+    asm("GOTO BlueFF");
+    asm("GOTO Blue00");
+    asm("BlueFF:");
+    asm("MOVLW  0xFF");
+    asm("MOVWF  _LEDB_Value_Tmp");
+    asm("GOTO endOfColorset");
+    asm("Blue00:");
+    asm("CLRF    _LEDB_Value_Tmp");
+
+    asm("endOfColorset:");
+    for(unsigned char i=0;i <LED_Length;i++){
+        LED_R[i] = LEDR_Value_Tmp;
+        asm("RRF _LEDR_Value_Tmp,1");
+        
+        LED_G[i] = LEDG_Value_Tmp ;
+        asm("RRF _LEDG_Value_Tmp,1");
+        
+        LED_B[i] = LEDB_Value_Tmp ;
+        asm("RRF _LEDB_Value_Tmp,1");
     }
+}
+
+void Led_Scroll_UP(){
+    unsigned char i;
+    LEDR_Value_Tmp = LED_R[0];
+    LEDG_Value_Tmp = LED_G[0];
+    LEDB_Value_Tmp = LED_B[0];
+    for(i = 0 ;i <  LED_Length ;i++){
+        LED_R[i] = LED_R[i + 1];
+        LED_G[i] = LED_G[i + 1];
+        LED_B[i] = LED_B[i + 1];
+    }
+    LED_R[9] = LEDR_Value_Tmp;
+    LED_G[9] = LEDG_Value_Tmp;
+    LED_B[9] = LEDB_Value_Tmp;
+}
+
+void Led_Scroll_DOWN(){
+    unsigned char i;
+    LEDR_Value_Tmp = LED_R[9];
+    LEDG_Value_Tmp = LED_G[9];
+    LEDB_Value_Tmp = LED_B[9];
+    for(i = LED_Length-1 ;i >  0 ;i--){
+        LED_R[i] = LED_R[i - 1];
+        LED_G[i] = LED_G[i - 1];
+        LED_B[i] = LED_B[i - 1];
+    }
+    LED_R[0] = LEDR_Value_Tmp;
+    LED_G[0] = LEDG_Value_Tmp;
+    LED_B[0] = LEDB_Value_Tmp;
 }
